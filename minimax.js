cf028695ae5f5a27d8fe08897d0c48ab47218110
bernardnguyen <bernardquynguyen@gmail.com>
2017-09-01 13:55:56 -0700

    minimax evaluation complete

--- minimax.js
+++ minimax.js
@@ -1,17 +1,15 @@
 var minimax = {
     'name': 'Minimax',
     'next': function (boardRep) {
-        var depth = 1;
+        var depth = 2;
         var gameTree = createTree(boardRep, -1, depth, true);
 
         gameTree.score = MINIMAX(gameTree, depth, true);
 
         var bestCol = -1;
         var maxScore = -1000;
-        console.log("SCORES");
         for (var k = 0; k < gameTree.children.length; k++) {
             var score = gameTree.children[k].score;
-            console.log(k + " " + score);
             if (score > maxScore) {
                 maxScore = score;
                 bestCol = gameTree.children[k].crumb;
@@ -31,7 +29,7 @@ function MINIMAX(node, depth, isMaximizingPlayer) {
         for (var i = 0; i < node.children.length; i++) {
             var value = MINIMAX(node.children[i], depth - 1, false);
             bestVal = Math.max(value, bestVal);
-            node.children[i].score = bestVal;
+            node.children[i].score = value;
         }
         return bestVal;
     } else {
@@ -39,148 +37,145 @@ function MINIMAX(node, depth, isMaximizingPlayer) {
         for (var i = 0; i < node.children.length; i++) {
             var value = MINIMAX(node.children[i], depth - 1, true);
             bestVal = Math.min(value, bestVal);
-            node.children[i].score = bestVal;
+            node.children[i].score = value;
         }
         return bestVal;
     }
 }
 
 function evaluate(board) {
-    print(board);
-
-    var output = 0;
-    
-    output += checkHorizontalLinks(board);
-    output += checkVerticalLinks(board);
-    output += checkDDLinks(board);
-    output += checkDULinks(board);
-
-    console.log("SCORE: " + output);
-    return output;
+    var score = 0;
+    score += gaussian(board);
+    score += checkFor3(board, 4);
+    score += checkFor4(board, 5);
+    return score;
 }
 
-function checkHorizontalLinks(board){
-    var output = 0;
-    var runningCount = 1;
-    var score = 1;
-    var currentChar = ' ';
+var evaluationBoard = [
+    [1,2,3,5,9,10],
+    [2,3,6,10,14,17],
+    [5,10,12,15,18,20],
+    [8,13,18,20,23,25],
+    [5,10,12,15,18,20],
+    [2,3,6,10,14,17],
+    [1,2,3,5,9,10]
+];
 
-    for(var j=0; j<6; j++){
-        for(var i=1; i<7; i++){
-            if (board[i][j] != ' '){    
-                curentChar = board[i][j];
-                if(board[i][j]==board[i-1][j]){
-                    runningCount+=1;
-                    score *= 5;
-
-                    if (i+1<7){
-                        if(board[i+1][j]!=currentChar){
-                            output+= (currentChar == 'X') ? -score : score;
-                            runningCount = 1;
-                            score = 1;
-                        }
-                    }
-                }
-            } else{
-                runningCount = 1;
-                score = 1;
-            }
-        }
-    }
-    console.log("HORIZONTAL SCORE: " + output);
-    return output;
-}
-
-function checkVerticalLinks(board){
-    var output = 0;
-    var runningCount = 1;
-    var score = 1;
-    var currentChar = ' ';
-
+function gaussian(board){
+    var sum = 0;
     for(var i=0; i<7; i++){
-        for(var j=1; j<6; j++){
-            if (board[i][j] != ' '){    
-                curentChar = board[i][j];
-                if(board[i][j]==board[i][j-1]){
-                    runningCount+=1;
-                    score *= 5;
-                    if (j+1<6){
-                        if(board[i][j+1]!=currentChar){
-                            output+= (currentChar == 'X') ? -score : score;
-                            runningCount = 1;
-                            score = 1;
-                        }
-                    }
-                } 
-            } else{
-                runningCount = 1;
-                score = 1;
-            }
-        }
-        score = 1;
-    }
-    console.log("VERTICAL SCORE: " + output);
-    return output;
-}
-
-function checkDDLinks(board){
-    var output = 0;
-    var runningCount = 1;
-    var score = 1;
-    var currentChar = ' ';
-
-    for(var i=1; i<7; i++){
-        for(var j=1; j<6; j++){
-            if (board[i][j] != ' '){    
-                curentChar = board[i][j];
-                if(board[i][j]==board[i-1][j-1]){
-                    runningCount+=1;
-                    score *= 5;
-                    if (i+1<7 && j+1<6){
-                        if(board[i+1][j+1]!=currentChar){
-                            output+= (currentChar == 'X') ? -score : score;
-                            runningCount = 1;
-                            score = 1;
-                        }
-                    }
-                } 
-            } else{
-                runningCount = 1;
-                score = 1;
+        for(var j=0; j<6; j++){
+            if(board[i][j] == 'O'){
+                sum+= evaluationBoard[i][j];
+            } else if(board[i][j] == 'X'){
+                sum-= evaluationBoard[i][j];
             }
         }
     }
-    console.log("DD SCORE: " + output);
-    return output;
+
+    return sum;
 }
 
-function checkDULinks(board){
+function checkFor3(board, weight){
     var output = 0;
-    var runningCount = 1;
-    var score = 1;
-    var currentChar = ' ';
 
-    for(var i=1; i<7; i++){
-        for(var j=4; j>-1; j--){
-            if (board[i][j] != ' '){    
-                curentChar = board[i][j];
-                if(board[i][j]==board[i-1][j+1]){
-                    runningCount+=1;
-                    score *= 5;
-                    if (i+1<7 && j-1>-1){
-                        if(board[i+1][j-1]!=currentChar){
-                            output+= (currentChar == 'X') ? -score : score;
-                            runningCount = 1;
-                            score = 1;
-                        }
-                    }
+    //CHECK HORIZONTAL
+    for(var j=0; j<6; j++){
+        for(var i=0; i<5; i++){
+            if(board[i][j]!=' '){
+                if(board[i+1][j]==board[i][j] && board[i+2][j]==board[i][j]){
+                    var score = Math.pow(weight, 3);
+                    output += (board[i][j] == 'X') ? -score : score;
+                    i+=3;
                 }
-            } else{
-                runningCount = 1;
-                score = 1;
             }
         }
     }
-    console.log("DU SCORE: " + output);
+    //CHECK VERTICAL
+    for(var i=0; i<7; i++){
+        for(var j=0; j<4; j++){
+            if(board[i][j]!=' '){
+                if(board[i][j+1]==board[i][j] && board[i][j+2]==board[i][j]){
+                    var score = Math.pow(weight, 3);
+                    output += (board[i][j] == 'X') ? -score : score;
+                    j+=3;
+                }
+            }
+        }
+    }
+    //CHECK DIAGONAL DOWN
+    for(var i=0; i<5; i++){
+        for(var j=0; j<4; j++){
+            if(board[i][j]!=' '){
+                if(board[i+1][j+1]==board[i][j] && board[i+2][j+2]==board[i][j]){
+                    var score = Math.pow(weight, 3);
+                    output += (board[i][j] == 'X') ? -score : score;
+                }
+            }
+        }
+    }
+    //CHECK DIAGONAL UP
+    for(var i=0; i<5; i++){
+        for(var j=2; j<6; j++){
+            if(board[i][j]!=' '){
+                if(board[i+1][j-1]==board[i][j] && board[i+2][j-2]==board[i][j]){
+                    var score = Math.pow(weight, 3);
+                    output += (board[i][j] == 'X') ? -score : score;
+                }
+            }
+        }
+    }
+
+    return output;
+}
+
+function checkFor4(board, weight){
+    var output = 0;
+
+    //CHECK HORIZONTAL
+    for(var j=0; j<6; j++){
+        for(var i=0; i<4; i++){
+            if(board[i][j]!=' '){
+                if(board[i+1][j]==board[i][j] && board[i+2][j]==board[i][j] && board[i+3][j]==board[i][j]){
+                    var score = Math.pow(weight, 4);
+                    output += (board[i][j] == 'X') ? -score : score;
+                }
+            }
+        }
+    }
+    //CHECK VERTICAL
+    for(var i=0; i<7; i++){
+        for(var j=0; j<3; j++){
+            if(board[i][j]!=' '){
+                if(board[i][j+1]==board[i][j] && board[i][j+2]==board[i][j] && board[i][j+3]==board[i][j]){
+                    var score = Math.pow(weight, 4);
+                    output += (board[i][j] == 'X') ? -score : score;
+                }
+            }
+        }
+    }
+    //CHECK DIAGONAL DOWN
+    for(var i=0; i<4; i++){
+        for(var j=0; j<3; j++){
+            if(board[i][j]!=' '){
+                if(board[i+1][j+1]==board[i][j] && board[i+2][j+2]==board[i][j] && board[i+3][j+3]==board[i][j]){
+                    var score = Math.pow(weight, 4);
+                    output += (board[i][j] == 'X') ? -score : score;
+                }
+            }
+        }
+    }
+    //CHECK DIAGONAL UP
+    for(var i=0; i<4; i++){
+        for(var j=3; j<6; j++){
+            if(board[i][j]!=' '){
+                if(board[i+1][j-1]==board[i][j] && board[i+2][j-2]==board[i][j] && board[i+3][j-3]==board[i][j]){
+                    var score = Math.pow(weight, 4);
+                    output += (board[i][j] == 'X') ? -score : score;
+                }
+            }
+        }
+    }
+
     return output;
 }
